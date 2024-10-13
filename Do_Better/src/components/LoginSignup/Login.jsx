fb69872b21785cdd0e9eafe4f3ff7820158b551e
Sriya Medarametla <sriyamed1@gmail.com>
2024-10-13 09:00:46 -0400

    Authorization updated

--- Do_Better/src/components/LoginSignup/Login.jsx
+++ Do_Better/src/components/LoginSignup/Login.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './Login.css'
+
+import email_icon from '../Assets/email.png'
+import lock_icon from '../Assets/password.png'
+import person_icon from '../Assets/person.png'
+
+
+
 const Login = () => {
+
+  const handleLogin = () => {
+    // Redirect to a new page (e.g., "/home" for localhost:5173/home)
+    window.location.href = '/home';  // Change this to the desired page path
+  };
+
   return (
-    <div>
-      <Login/>
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>Sign Up</div>
+        <div className='underline'></div>
+      </div>
+      <div className='inputs'>
+        {/*first pic */}
+        <div className='input'>
+          <img src={person_icon} alt=''/>
+          <input type='text'/>
+          {/*second pic */}
+        </div>
+        <div className='input'>
+          <img src={email_icon} alt=''/>
+          <input type='email'/>
+        </div>
+          {/*third pic */}
+        <div className='input'>
+          <img src={lock_icon} alt=''/>
+          <input type='password'/>
+        </div>
+      </div>
+      <div className="forgot-password">Forgot Password</div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit" onClick={handleLogin}>Login</div>
+      </div>
     </div>
   )
 }
--- Do_Better/src/main.jsx
+++ Do_Better/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Correct import for createRoot
 import App from './App';
+import Login from './components/LoginSignup/Login.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
